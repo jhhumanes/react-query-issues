14944d700ebd469d436167797ecddcc9c78245f2
José Humanes Humanes <1357660+jhhumanes@users.noreply.github.com>
2023-03-03 05:09:48 +0100

    Paginación clásica.

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
     headers: {
-        Authorization: 'Bearer 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', // fine-grained personal access tokens
+        Authorization: 'Bearer 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', // fine-grained personal access tokens
     },
 });
--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -1,14 +1,16 @@
+import { useState, useEffect } from "react";
+import { useQuery } from '@tanstack/react-query';
 import { githubApi } from "../../api/githubApi";
 import { Issue, State } from "../interfaces";
-import { useQuery } from '@tanstack/react-query';
 import { sleep } from '../../helpers/sleep';
 
 interface Props {
     state?: State;
     labels: string[];
+    page?: number;
 }
 
-const getIssues = async (labels: string[] = [], state?: State): Promise<Issue[]> => {
+const getIssues = async ({ labels, state, page = 1 }: Props): Promise<Issue[]> => {
     await sleep(2);
 
     const params = new URLSearchParams();
@@ -19,7 +21,7 @@ const getIssues = async (labels: string[] = [], state?: State): Promise<Issue[]>
         const labelString = labels.join(',');
         params.append('labels', labelString);
     }
-    params.append('page', '1');
+    params.append('page', page.toString());
     params.append('per_page', '5');
 
     const { data } = await githubApi<Issue[]>('/issues', { params });
@@ -27,10 +29,47 @@ const getIssues = async (labels: string[] = [], state?: State): Promise<Issue[]>
 }
 
 export const useIssues = ({ state, labels }: Props) => {
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+      setPage(1);
+    }, [state, labels])
+
     const issuesQuery = useQuery(
-        ['issues', { state, labels }], // un objeto como parte de la clave de la caché sin que importe el orden de los datos del objeto
-        () => getIssues(labels, state)
+        ['issues', { state, labels, page }], // un objeto como parte de la clave de la caché sin que importe el orden de los datos del objeto
+        () => getIssues({ labels, state, page })
+        // {
+        //     onSuccess(data) {
+                
+        //     },
+        //     onError(err) {
+                
+        //     },
+        // }
     );
 
-    return { issuesQuery };
+    const nextPage = () => {
+        if (issuesQuery.data?.length === 0){
+            return;
+        }
+        setPage(page + 1);
+    }
+
+    const prevPage = () => {
+        if (page > 1){
+            setPage(page - 1);
+        }
+    }
+
+    return { 
+        // Properties
+        issuesQuery,
+
+        // Getters
+        page: issuesQuery.isFetching ? 'Cargando...' : page,
+
+        // Methods
+        nextPage,
+        prevPage,
+    };
 }
